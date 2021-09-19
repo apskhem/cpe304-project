db528e917dfbdab9678d44d7fa724ed243de060b
Apisit Ritreungroj <apisit_lon@hotmail.com>
2021-09-19 12:50:50 +0700

    Complete instructions

--- src/instruction.cpp
+++ src/instruction.cpp
@@ -10,22 +10,32 @@ public:
     }
 
     unsigned int opcode() {
-        return (*this).raw >> 22;
+        return (*this).get_bin_range(24, 22);
     }
 
     unsigned int rs1() {
-        return 0;
+        return (*this).get_bin_range(21, 19);
     }
 
     unsigned int rs2() {
-        return 0;
+        return (*this).get_bin_range(18, 16);
     }
 
     unsigned int rd() {
-        return 0;
+        return (*this).get_bin_range(2, 0);
     }
 
     short offset() {
         return (short) (*this).raw;
+    }
+
+private:
+    int get_bin_range(int start, int end) {
+        int sll = 32 - (start + 1);
+        int srll = sll + end;
+
+        int res = ((*this).raw << sll) >> srll;
+
+        return res;
     }
 };
--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -4,47 +4,96 @@
 
 using namespace std;
 
-void interpret(unsigned int* const mem, int* const regs, int* cp) {
-    unsigned int bin_instr = mem[*cp];
+void interpret(unsigned int* const mem, int* const regs, int* pc) {
+    unsigned int bin_instr = mem[*pc];
     Instruction instr(bin_instr);
 
     switch (instr.opcode()) {
         // add (R-type)
         case 0b000: {
-            *cp = *cp + 1;
+            int rs1 = instr.rs1();
+            int rs2 = instr.rs2();
+            int rd = instr.rd();
+
+            regs[rd] = rs1 + rs2;
+
+            *pc = *pc + 1;
             break;
         }
         // nand (R-type)
         case 0b001: {
-            *cp = *cp + 1;
+            int rs1 = instr.rs1();
+            int rs2 = instr.rs2();
+            int rd = instr.rd();
+
+            regs[rd] = ~(rs1 & rs2);
+
+            *pc = *pc + 1;
             break;
         }
         // lw (I-type)
         case 0b010: {
-            *cp = *cp + 1;
+            int rs1 = instr.rs1();
+            int rs2 = instr.rs2();
+            int offset = instr.offset();
+            int rs1_val = regs[rs1];
+            
+            regs[rs2] = mem[rs1_val + offset];
+
+            *pc = *pc + 1;
             break;
         }
         // sw (I-type)
         case 0b011: {
-            *cp = *cp + 1;
+            int rs1 = instr.rs1();
+            int rs2 = instr.rs2();
+            int offset = instr.offset();
+            int rs1_val = regs[rs1];
+
+            mem[rs1_val + offset] = regs[rs2];
+
+            *pc = *pc + 1;
             break;
         }
         // beg (I-type)
         case 0b100: {
+            int rs1 = instr.rs1();
+            int rs2 = instr.rs2();
+            int offset = instr.offset();
+            int rs1_val = regs[rs1];
+            int rs2_val = regs[rs2];
+
+            if (rs1_val == rs2_val) {
+                *pc = *pc + 1;
+            }
+            else {
+                *pc = *pc + offset;
+            }
+
             break;
         }
         // jalr (J-type)
         case 0b101: {
+            int rs1 = instr.rs1();
+            int rs2 = instr.rs2();
+            int rs1_val = regs[rs1];
+            
+            // store pc + 1
+            regs[rs2] = *pc + 1;
+
+            // perform jump
+            *pc = rs1_val;
+
             break;
         }
         // halt (O-type)
         case 0b110: {
-            *cp = -1;
+            *pc = -1;
             break;
         }
         // noop (O-type)
         case 0b111: {
-            *cp = *cp + 1;
+            *pc = *pc + 1;
             break;
         }
         default: {
